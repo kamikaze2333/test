c7ce54b63cb849b8cb37ee2473f55960c6dbfc82
kamikaze2333 <henghuic1@gmail.com>
2025-11-11 14:55:27 +0800

    test

--- Project1/Project1/test.cpp
+++ Project1/Project1/test.cpp
@@ -22,18 +22,22 @@ int divide(int a, int b, int* result)
 }
 */
 #include <stdio.h>
+#include <string.h>
+#include <windows.h>
 int main()
 {
+	char arr1[] = "welcome to hit!!!!!!!";
+	char arr2[] = "*********************";
 	int left = 0;
-	int right = strlen (arr1) - 1;
-	char arr1[] = { 1 1 1 1 1 1};
-	char arr2[] = { * * * * * *};
-	while (left <= right) {
-		arr1[left] = arr2[left];
-		arr2[right] = arr2[right];
+	int right = strlen(arr1) - 1;
+	while (left <= right)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		Sleep(1000);
 		left++;
 		right--;
 	}
-	printf("%d", arr1[]);
 	return 0;
 }
